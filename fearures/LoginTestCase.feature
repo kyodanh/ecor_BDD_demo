01d197f32778de713479d1ac09d8740679e9fb96
kyodanh <kyodanh@gmail.com>
2023-02-28 11:27:39 +0700

    kiểm tra lại phầm method và action

--- fearures/LoginTestCase.feature
+++ fearures/LoginTestCase.feature
@@ -2,7 +2,7 @@ Feature: Login test case
 
   Scenario Outline: Thực hiện login và hệ thống và kiểm tra user
     Given Thực hiện mở trang web
-    When Thực hiện nhập username và password
+    When Thực hiện nhập "username" và "password"
     And Thực hiện bấm vào nút Login
     And Thực hiện kiểm tra xem username hiển thị đúng
     Then thực hiện kiểm tra có hiển thị avatar user
--- steps/Login.py
+++ steps/Login.py
@@ -1,7 +1,10 @@
+import time
+
 from behave import *
 from selenium import webdriver
-
-
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import get_remote_connection
 
 
 @given(u'Thực hiện mở trang web')
@@ -9,18 +12,26 @@ def step_impl(context):
     context.driver = webdriver.Chrome(executable_path="chromedriver.exe")
     context.driver.implicitly_wait(10)
     context.driver.maximize_window()
-    context.driver.get("https://www.saucedemo.com/")
+    context.driver.get("https://magento.softwaretestingboard.com/")
 
 
-@when(u'Thực hiện nhập username và password')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When Thực hiện nhập username và password')
+@when(u'Thực hiện nhập "{username}" và "{password}"')
+def step_impl(context,username,password):
+    driver = context.driver
+    driver.find_element(By.XPATH,"//li[2]/a").click()
+    time.sleep(2)
+    driver.find_element(By.XPATH,"//*[@id='email']").send_keys(username)
+    time.sleep(2)
+    driver.find_element(By.XPATH,"//*[@id='pass']").send_keys(password)
+    time.sleep(2)
+
+
 
 
 @when(u'Thực hiện bấm vào nút Login')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Thực hiện bấm vào nút Login')
-
+    driver = context.driver
+    driver.find_element(By.XPATH,"//*[@id='send2']/span").click()
 
 @when(u'Thực hiện kiểm tra xem username hiển thị đúng')
 def step_impl(context):
